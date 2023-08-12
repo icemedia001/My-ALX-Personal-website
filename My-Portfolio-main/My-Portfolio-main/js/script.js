11499b16fb64a807067ab28fafd52d4acecca16f
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2023-08-12 02:12:33 +0100

    Add files via upload

---
+++ My-Portfolio-main/My-Portfolio-main/js/script.js
@@ -0,0 +1,8 @@
+/* ==== typing animation ==== */
+var typed = new Typed(".typing",
+{
+    strings:["ALX Learner", "an AWS DevOps Engineer Enthusiast", "a Backend Web Developer", " an ALX learner"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+});
